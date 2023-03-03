e4f045d1739d5718c6d12e9165642f153b8407a3
bunnany <45374304+bunnany@users.noreply.github.com>
2023-03-03 14:06:15 +1300

    Added class score lists, ask user name

--- class_scores.py
+++ class_scores.py
@@ -1,4 +1,11 @@
 ##
 #
+# Author: NYB
 
-print("Hello, World!")
+# Lists of class scores
+class_A = [44, 33, 28, 47, 12, 28, 32, 31, 11, 39, 40, 26, 36]
+class_B = [19, 26, 38, 31, 30, 42, 44, 14, 27, 43, 46, 49, 24, 26, 36]
+
+# Ask user for name
+name = input("Enter name: ")
+print("Hello, ", name)
